2e6723417bd3105b425c5fa0fa7231bda6467774
SuhailHasan <64515577+SuhailHasan@users.noreply.github.com>
2020-04-29 10:20:26 +0530

    Update NumberInfo.py

--- NumberInfo.py
+++ NumberInfo.py
@@ -1,5 +1,15 @@
 from phonenumbers import carrier, geocoder
 import phonenumbers
+
+print("""
+    _   __                __             ____      ____    
+   / | / /_  ______ ___  / /_  ___  ____/  _/___  / __/___ 
+  /  |/ / / / / __ `__ \/ __ \/ _ \/ ___/ // __ \/ /_/ __ \
+ / /|  / /_/ / / / / / / /_/ /  __/ / _/ // / / / __/ /_/ /
+/_/ |_/\__,_/_/ /_/ /_/_.___/\___/_/ /___/_/ /_/_/  \____/ 
+                                                           
+                                                    (Made by Suhail)
+""")
 phoneNo = input("Enter Phone Number: ")
 ph =phonenumbers.parse(f"phoneNo")
 vn = carrier.name_for_valid_number(ph,"en")
